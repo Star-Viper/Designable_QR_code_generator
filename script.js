d475e5f5a58d9832bfcdacbf4bc8ec824cf8045d
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2023-12-25 11:55:15 +0530

    added share options

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function generateQRCode() {
     if (inputText.trim() !== '') {
       // Clear previous QR code
       qrcodeContainer.innerHTML = '';
-  
       // Generate QR code
       var qrcode = new QRCode(qrcodeContainer, {
         text: inputText,
@@ -16,4 +15,21 @@ function generateQRCode() {
       alert('Please enter text before generating the QR code.');
     }
   }
+
+  function shareOnFacebook() {
+    var qrCodeImage = document.getElementById('qrcode').getElementsByTagName('img')[0].src;
+    var facebookShareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(qrCodeImage);
+    openSocialMediaPopup(facebookShareUrl);
+  }
+  
+  function shareOnTwitter() {
+    var qrCodeImage = document.getElementById('qrcode').getElementsByTagName('img')[0].src;
+    var twitterShareUrl = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(qrCodeImage) + '&text=Check%20out%20this%20QR%20Code';
+    openSocialMediaPopup(twitterShareUrl);
+  }
+  
+  function openSocialMediaPopup(url) {
+    window.open(url, '_blank', 'width=600,height=400');
+  }
+  
   
